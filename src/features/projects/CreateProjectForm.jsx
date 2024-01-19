240cdef1d02c3259dd90dafda49ea96e54229aad
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-01-19 20:22:54 +0330

    feat:createProjectForm added plus react hook form

--- src/features/projects/CreateProjectForm.jsx
+++ src/features/projects/CreateProjectForm.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-
+import { data } from 'autoprefixer'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 function CreateProjectForm({open}) {
-  return open && <div className='backdrop'></div>
+const {register,handleSubmit} = useForm()
+const [form,setForm] = useState({})
+const onSubmit = (data) => {setForm(data),console.log(data)}
+  return open && <div className='backdrop flex items-center justify-center'>
+    <div className='bg-white w-[calc(100vw-4rem)] h-64 max-h-[800px] rounded-lg shadow-xl max-w-screen-sm'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      </form>
+    </div>
+  </div>
 }
 
 export default CreateProjectForm
