5fe9e7be78adb4e7d27bd069b34768286f48bed3
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-02-16 19:21:57 +0330

    freelancer dashboard added

--- src/features/Freelancer/FreelancerDashboard.jsx
+++ src/features/Freelancer/FreelancerDashboard.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { DashboardCard } from "../../pages/OwnerDashboard";
 
 function FreelancerDashboard() {
-  return (
-    <div>FreelancerDashboard</div>
-  )
+  return <div className="w-full h-full bg-sky-50 dark:bg-slate-700">
+    hello
+  </div>;
 }
 
-export default FreelancerDashboard
+export default FreelancerDashboard;
