89bf841332c78182a6aed64b5c6fe81323fd01df
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-02-17 20:42:10 +0330

    feat:chart.js added

---
+++ src/UI/Chart.jsx
@@ -0,0 +1,31 @@
+const data = {
+    labels: [
+        "کل پروژه ها",
+        "پروژه های به اتمام رسیده",
+        "پروژه های درحال انجام"
+    ],
+    datasets:[{
+        label:"آمار",
+        data:[12,2,0],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+    }]
+}
+const config = {
+    type:'pie',
+    data:data
+}
+import React from 'react'
+import { PieChart } from 'recharts'
+
+function Chart() {
+  return (
+    <PieChart/>
+  )
+}
+
+export default Chart
