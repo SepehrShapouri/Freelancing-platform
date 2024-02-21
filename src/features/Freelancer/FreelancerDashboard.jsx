e12cf28f4d29133a3ecf0742f2550365cdc0c09c
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-02-21 15:13:16 +0330

    initial commit

--- src/features/Freelancer/FreelancerDashboard.jsx
+++ src/features/Freelancer/FreelancerDashboard.jsx
@@ -5,7 +5,7 @@ import Chart from "../../UI/Chart";
 function FreelancerDashboard() {
   return <div className="w-full h-full bg-sky-50 dark:bg-slate-700 p-4">
 <h2 className="text-2xl text-cyan-800 dark:text-indigo-400 font-bold">داشبورد</h2>
-
+con
   </div>;
 }
 
